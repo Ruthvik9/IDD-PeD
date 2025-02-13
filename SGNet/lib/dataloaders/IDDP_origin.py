b8501a1f560f8b3bcfd95a420503016e9c349aa0
Ruthvik9 <qwertyruthvik@gmail.com>
2025-02-13 13:03:38 +0530

    changing interface file

--- SGNet/lib/dataloaders/IDDP_origin.py
+++ SGNet/lib/dataloaders/IDDP_origin.py
@@ -78,10 +78,10 @@ class IDDPedestrian():
         #                   'test': ['set03'],
         #                   'all': ['set01', 'set02', 'set03',
         #                           'set04', 'set05', 'set06']}
-        image_set_nums = {'train': ['gp_set_0001','gp_set_0002','gp_set_0004','gp_set_0006'],
-                          'val': ['gp_set_0003','gp_set_0005'],
-                          'test': ['gp_set_0003','gp_set_0005'],
-                          'all': ['gp_set_0001', 'gp_set_0002','gp_set_0003','gp_set_0004','gp_set_0005','gp_set_0006']}
+        image_set_nums = {'train': ['gp_set_0001','gp_set_0002','gp_set_0004','gp_set_0006','gp_set_0007'],
+                          'val': ['gp_set_0001','gp_set_0002','gp_set_0004','gp_set_0006','gp_set_0007'],
+                          'test': ['gp_set_0003','gp_set_0005','gp_set_0008','gp_set_0009'],
+                          'all': ['gp_set_0001', 'gp_set_0002','gp_set_0003','gp_set_0004','gp_set_0005','gp_set_0006','gp_set_0007','gp_set_0008','gp_set_0009']}
         return image_set_nums[image_set]
 
     def _get_image_path(self, sid, vid, fid):
